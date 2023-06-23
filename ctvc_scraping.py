d465ad647f5eaec82f95be7926f184285a4b4715
ELLIOT THOBURN <elliot.weldon.thoburn@gmail.com>
2023-06-23 10:07:58 -0400

    init

---
+++ ctvc_scraping.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+
+#returns deals, exits, and links, and date in a dictionary
+def scrape_data(url):
+    scraping_data = {'deals':[],'exits':[],'links':[],'date':""}
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    #print out the date at the top
+    scraping_data['date'] += soup.find("time", class_="mt-1 sm:mt-0").text#["datetime"]
+    data = soup.find("h2", string=lambda text: "Deals of the Week" in text)
+    next_sibling = data.next_sibling
+    other_fundings = False
+
+    while next_sibling is not None:
+        data = next_sibling
+
+        #skips over any elements with id that is not p, h2, or h3
+        if (data.name in ["p", "h2", "h3"]):
+            #stop at exits and new funds for now
+            if ("Exits" in data.text) or ("New Funds" in data.text): 
+                break
+
+            #print out each deal and its link
+            if ("p" in data.name): 
+                link = data.find("a")
+
+                #only print out debt financing deals if it is before other fundings
+                if ("debt" not in data.text) or (other_fundings == False):
+                    if link is not None: scraping_data['links'].append(link["href"])
+                    else: scraping_data['links'].append(None)
+                    scraping_data['deals'].append(data.text)
+            if ("Other Fundings" in data.text): other_fundings = True
+
+        next_sibling = next_sibling.next_sibling
+
+    #get exits
+    data = soup.find("h3", string=lambda text: "Exits" in text)
+    next_sibling = data.next_sibling
+    new_fund_terms = ['launched','raised','held']
+    new_fund = False
+
+
+    while (next_sibling != None) and (next_sibling.name == "p"):
+        data = next_sibling
+        for term in new_fund_terms:
+            if term in data.text:
+                new_fund = True 
+        if new_fund == True:
+            break
+        scraping_data['exits'].append(data.text)
+        #add exit links to the same list as deals
+        link = data.find("a")
+        scraping_data['links'].append(link["href"]) if link is not None else scraping_data['links'].append(None)
+        next_sibling = next_sibling.next_sibling
+    
+    return scraping_data
---
+++ ctvc_streamlit.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+import ctvc_scraping as scraping
+import ctvc_compiling as compiling
+
+#setup
+
+st.title("CTVC Newsletter Scraping")
+url = st.text_input('CTVC URL')
+openai_api_key = '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'
+
+@st.cache_data
+def scrape_and_compile(url,openai_api_key):
+    #scrape
+    scrape_output = scraping.scrape_data(url)
+    #compile
+    llm = compiling.set_up_llm(openai_api_key)
+    extracted_output = compiling.extract_deals(llm,scrape_output['deals'])
+    extracted_output += compiling.extract_exits(llm,scrape_output['exits'])
+    return compiling.ctvc_to_df(extracted_output,scrape_output['date'],scrape_output['links'])
+
+if st.button('get data'):
+    # scrape_output,extracted_output = scrape_and_compile(url,openai_api_key)
+    # st.write(scrape_output)
+    # st.write(extracted_output)
+    st.write(scrape_and_compile(url,openai_api_key))
